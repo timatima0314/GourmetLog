ec77c1bf2620284af51754dde9288ac353fdd9d8
timatima0314 <tima314@gmail.com>
2023-08-01 22:39:34 +0900

    add:store追加。

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -2,9 +2,9 @@ import "./bootstrap";
 import router from "./router";
 
 import { createApp, provide } from "vue";
-import App from"./App.vue";
-
+import App from "./App.vue";
+import { key, store } from "./store/store";
 const app = createApp(App);
 app.use(router);
-
+app.use(store, key);
 app.mount("#app");
---
+++ resources/ts/store/store.ts
@@ -0,0 +1,50 @@
+import { InjectionKey } from 'vue';
+import { createStore, Store, useStore as baseUseStore } from "vuex";
+import * as MutationTypes from "./mutationTypes";
+import { ADD_RESTAURANT_DETA } from './mutationTypes';
+
+type RestaurantData = {
+    name: string,
+    name_katakana: string,
+    review: number,
+    food_picture: string,
+    map_url: string,
+    comment: string,
+    tel: string,
+    user_id: number,
+};
+// stateの型定義
+type State = {
+    restaurantData: RestaurantData[];
+};
+
+// storeをprovide/injectするためのキー
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// store本体
+export const store = createStore<State>({
+    state: {
+        restaurantData: [
+            {
+                name: "",
+                name_katakana: "",
+                review: 1,
+                food_picture: '',
+                map_url: '',
+                comment: '',
+                tel: '',
+                user_id: 1,
+            }
+        ]
+    },
+    mutations: {
+        [MutationTypes.ADD_RESTAURANT_DETA](state, restaurantData: RestaurantData) {
+            state.restaurantData.push(restaurantData);
+        }
+    }
+});
+
+// useStoreを使う時にキーの指定を省略するためのラッパー関数
+export const useStore = () => {
+    return baseUseStore(key);
+}
