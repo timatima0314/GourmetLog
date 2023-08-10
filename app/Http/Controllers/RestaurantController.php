8fb0533c062394fbb23ab02533137c7108fcdf2e
timatima0314 <tima314@gmail.com>
2023-08-10 21:25:45 +0900

    add:Restaurant_DB CRUD追加。

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
+use Illuminate\Support\Facades\Storage;
 
 class RestaurantController extends Controller
 {
@@ -20,7 +21,21 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        $restaurant = Restaurant::create($request->all());
+        $file_name = $request->file('food_picture')->getClientOriginalName();
+        $request->file('food_picture')->storeAs('public/', $file_name);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->name;
+        $restaurant->name_katakana = $request->name_katakana;
+        $restaurant->comment = $request->comment;
+        $restaurant->map_url = $request->map_url;
+        $restaurant->review = $request->review;
+        $restaurant->tel = $request->tel;
+        $restaurant->user_id = $request->user_id;
+        $restaurant->food_picture = $file_name;
+
+        $restaurant->save();
+
         return $restaurant
             ? response()->json($restaurant, 201)
             : response()->json([], 500);
@@ -37,9 +52,29 @@ class RestaurantController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function updated(Request $request,  $id)
     {
-        //
+        $restaurant = Restaurant::findorFail($id);
+        $file = $request->file('food_picture');
+        if (isset($file)) {
+            $path = $restaurant->food_picture;
+            Storage::disk('local')->delete('public/' . $path);  //大事
+            $file_name = $request->file('food_picture')->getClientOriginalName();
+            $restaurant->food_picture = $file_name;
+            $request->file('food_picture')->storeAs('public/', $file_name);
+        }
+        $restaurant->name = $request->name;
+        $restaurant->name_katakana = $request->name_katakana;
+        $restaurant->comment = $request->comment;
+        $restaurant->map_url = $request->map_url;
+        $restaurant->review = $request->review;
+        $restaurant->tel = $request->tel;
+        $restaurant->user_id = $request->user_id;
+
+        $restaurant->save();
+        return $restaurant
+            ? response()->json($restaurant, 201)
+            : response()->json([], 500);
     }
 
     /**
@@ -47,6 +82,10 @@ class RestaurantController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $restaurant = Restaurant::findorFail($id);
+        $restaurant->delete();
+        return $restaurant
+            ? response()->json($restaurant, 201)
+            : response()->json([], 500);
     }
 }
--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Restaurant extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'restaurants';
 
     protected $fillable = [
-        'user_id', 'name', 'name_katakana', 'review', 'food_picture', 'map_url', 'comment', 'tel'
+        'user_id', 'name', 'name_katakana', 'review', 'food_picture', 'map_url', 'comment', 'tel', 'deleted_at'
     ];
 }
